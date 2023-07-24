78918bd1bdfe5bb63917193fe6e1479e9e261a46
Blackstar84 <carlosalmada84@gmail.com>
2023-07-24 17:25:58 -0400

    se agrega la parte de create users

--- main.py
+++ main.py
@@ -15,10 +15,46 @@ class User(peewee.Model):
         database = database
         db_table = 'users'
         
+    def __str__(self):
+        return self.username    
+        
             
 if __name__ == '__main__':
     
     if User.table_exists():
         User.drop_table()
         
-    User.create_table()            
+    User.create_table()
+    
+    #Método 1
+    user1 = User(username='user1', email='user1@example.com', active=True)
+
+    """ print(user1)
+    print(user1.email)
+    print(user1.active) """
+    
+    user1.save()
+    
+    # Método 2
+    user2 = User()
+    user2.username = 'user2'
+    user2.email = 'user2@example.com'
+    user2.save()
+    
+    # Método 3
+    
+    values = {
+        'username': 'user3',
+        'email': 'user3@example.com'
+    }
+    
+    user3 = User(**values)
+    user3.save()
+    
+    user4 = User.create(username='user4', email='user4@example.com')
+    print(user4.id)
+    
+    
+    
+    
+    
