08a4075a667bb33ed6d1a0313e468307c7c2ad6a
Blackstar84 <carlosalmada84@gmail.com>
2023-07-31 14:20:57 -0400

    se agrega hasta la clase relación de uno a uno

--- main.py
+++ main.py
@@ -3,41 +3,45 @@ from datetime import datetime
 
 database = peewee.MySQLDatabase(database='pythondb', host='localhost', port=3306, user='root', passwd='root')
 
-class Author(peewee.Model):
-    name = peewee.CharField(max_length=50)
+class User(peewee.Model):
+    username = peewee.CharField(max_length=50)
+    email = peewee.CharField(max_length=50)
     
     class Meta:
         database = database
-        db_table = 'authors'
+        db_table = 'users'
+        
     def __str__(self) -> str:
-        return self.name
+        return self.username    
+    @property
+    def admin(self):
+        return self.admins.first()
     
-        
-class Book(peewee.Model):    
-    title = peewee.CharField(max_length=50)
-    author = peewee.ForeignKeyField(Author, backref='books')
+class Admin(peewee.Model):
+    permission_level = peewee.IntegerField(default=1)
+    user = peewee.ForeignKeyField(User, backref='admins', unique=True)
     
     class Meta:
         database = database
-        db_table = 'books'
-        
-    def __str__(self):
-        return self.title
-        
+        db_table = 'admins'
+
+    def __str__(self) -> str:
+        return 'Admin ' + str(self.id)
+
             
 if __name__ == '__main__':
     
-    database.drop_tables([Author, Book])
-    database.create_tables([Author, Book])
+    database.drop_tables([User, Admin])
+    database.create_tables([User, Admin])
     
-    author1 = Author.create(name='Stephen King')
+    user1 = User.create(username='User1', email='user1@example.com')
+    admin1 = Admin.create(permission_level=10, user=user1)
     
-    book1 = Book.create(title='It', author=author1)
-    book2 = Book.create(title='El resplandor', author=author1)
-    book3 = Book.create(title='Cujo', author=author1)
+    # print(admin1.user.username)
+    # print(admin1.user.email)
     
+    # print(user1.admins.first())
+    # print(user1.admin())
+    print(user1.admin)
     
-    print(author1.books)
-    
-    for book in author1.books:
-        print(book)
+    admin2 = Admin.create(permission_level=5, user=user1)
