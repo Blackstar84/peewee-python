7810de9654c70c856b92c23e6b4596bd0a1308cf
Blackstar84 <carlosalmada84@gmail.com>
2023-07-31 09:30:31 -0400

    se agrega la clase hasta relación uno a muchos

--- main.py
+++ main.py
@@ -3,10 +3,41 @@ from datetime import datetime
 
 database = peewee.MySQLDatabase(database='pythondb', host='localhost', port=3306, user='root', passwd='root')
 
-
-
+class Author(peewee.Model):
+    name = peewee.CharField(max_length=50)
+    
+    class Meta:
+        database = database
+        db_table = 'authors'
+    def __str__(self) -> str:
+        return self.name
+    
+        
+class Book(peewee.Model):    
+    title = peewee.CharField(max_length=50)
+    author = peewee.ForeignKeyField(Author, backref='books')
+    
+    class Meta:
+        database = database
+        db_table = 'books'
+        
+    def __str__(self):
+        return self.title
         
             
 if __name__ == '__main__':
-    pass
     
+    database.drop_tables([Author, Book])
+    database.create_tables([Author, Book])
+    
+    author1 = Author.create(name='Stephen King')
+    
+    book1 = Book.create(title='It', author=author1)
+    book2 = Book.create(title='El resplandor', author=author1)
+    book3 = Book.create(title='Cujo', author=author1)
+    
+    
+    print(author1.books)
+    
+    for book in author1.books:
+        print(book)
---
+++ main_hasta_relacion_uno_a_muchos.py
@@ -0,0 +1,10 @@
+import peewee
+from datetime import datetime
+
+database = peewee.MySQLDatabase(database='pythondb', host='localhost', port=3306, user='root', passwd='root')
+
+        
+            
+if __name__ == '__main__':
+    
+  
